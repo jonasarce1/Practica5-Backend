120c1b517e151260116dacfcd5a744f1ea7e6398
jonasarce1 <jonasarceg@gmail.com>
2023-12-14 22:25:22 +0100

    añadidos cronjobs

--- main.ts
+++ main.ts
@@ -5,9 +5,13 @@ import { Mutation } from "./resolvers/mutation.ts";
 import { Cliente } from "./resolvers/Cliente.ts";
 import { Conductor } from "./resolvers/Conductor.ts";
 import { Viaje } from "./resolvers/Viaje.ts";
+import { ClienteModel } from "./db/cliente.ts";
+import { ConductorModel } from "./db/conductor.ts";
+import { ViajeModel } from "./db/viaje.ts";
 
 import { typeDefs } from "./gql/schema.ts";
 import mongoose from "mongoose";
+import { daily } from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
 
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 const env = await load();
@@ -32,6 +36,53 @@ const server = new ApolloServer({
       Conductor,
       Viaje
     },
+});
+
+//Funcion que comprueba si hay tarjetas expiradas y las borra
+const checkExpirity = async () => {
+  const clientes = await ClienteModel.find();
+  const tarjetas = clientes.map(cliente => cliente.cards).flat();
+  const fechaActual = new Date();
+  const anyoActual = fechaActual.getFullYear();
+  const mesActual = fechaActual.getMonth(); //el mes puede ser el mismo
+
+  tarjetas.forEach(tarjeta => {
+      const mes = parseInt(tarjeta.expirity.split("/")[0]);
+      const anyo = parseInt(tarjeta.expirity.split("/")[1]);
+      if(anyo < anyoActual || (anyo == anyoActual && mes < mesActual)){
+          console.log("Borrando tarjeta expirada");
+          tarjeta.remove();
+      }
+  })
+}
+
+//Funcion que comprueba si hay viajes pasados y los borra
+const checkViajes = async () => {
+  const viajes = await ViajeModel.find();
+  viajes.forEach(async viaje => { 
+      if(Date.now() > viaje.date.getTime()){ 
+          //si el viaje ha pasado de la fecha actual se borra de la BBDD y se borran sus asociaciones
+          const cliente = await ClienteModel.findById(viaje.client).populate('travels').exec();
+          const conductor = await ConductorModel.findById(viaje.driver).populate('travels').exec();
+          if(cliente && conductor){
+              //se borran las asociaciones
+              cliente.travels.filter(viaje => viaje._id != viaje._id);
+              await cliente.save();
+              conductor.travels.filter(viaje => viaje._id != viaje._id);
+              await conductor.save();
+          }            
+          await ViajeModel.findByIdAndDelete(viaje._id).exec();
+          console.log("Borrando viaje expirado");
+      }
+  })
+}
+
+//CronJob que se ejecuta cada dia comprobando si hay tarjetas expiradas y viajes pasadosm url con documentacion: https://deno.land/x/deno_cron@v1.0.0
+daily(() => {
+  console.log("Comprobando tarjetas expiradas");
+  checkExpirity();
+  console.log("Comprobando viajes pasados");
+  checkViajes();
 });
 
 const { url } = await startStandaloneServer(server);
